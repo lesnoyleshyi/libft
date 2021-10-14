6bc6b461a4d05ba419fd6147a32cc1ebd81ace1a
stycho <stycho@student.21-school.ru>
2021-10-15 01:32:52 +0300

    remove OBJDIR from Makefile

--- Makefile
+++ Makefile
@@ -14,8 +14,6 @@ SRCS	=	ft_isalpha.c	ft_isdigit.c	ft_isalnum.c	ft_isascii.c\
 
 OBJS	=	$(SRCS:.c=.o)
 
-OBJDIR	:=	./.objs
-
 FLAGS	:=	-Wall -Werror -Wextra
 
 %.o		:	%.c
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,40 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stycho <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By:  <stycho>                                   +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/08 20:53:14 by stycho            #+#    #+#             */
-/*   Updated: 2021/10/10 20:59:53 by stycho           ###   ########.fr       */
+/*   Created: 2021/10/15 01:05:44 by                   #+#    #+#             */
+/*   Updated: 2021/10/15 01:08:08 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include <stddef.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	char	*h_pos;
-	char	*n_pos;
-	char	*f_oc;
 
-	if (*needle == '\0')
-		return ((char *)haystack);
-	h_pos = (char *)haystack;
-	f_oc = NULL;
-	while (*h_pos != '\0' && len)
-	{
-		n_pos = (char *)needle;
-		if (*h_pos == *n_pos)
-			f_oc = h_pos;
-		while (*h_pos == *n_pos)
-		{
-			if (*(n_pos++ + 1) == '\0')
-				return (f_oc);
-			h_pos++;
-			if (--len == 0)
-				return (NULL);
-		}
-		h_pos++;
-		len--;
-	}
-	return (NULL);
+
 }
