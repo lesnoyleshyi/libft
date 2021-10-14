bb6f62117a422c266a231e408223250572e4a654
stycho <stycho@student.21-school.ru>
2021-10-14 12:42:44 +0300

    ft_substr modified

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,12 @@
 /*   By: stycho <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 18:49:37 by stycho            #+#    #+#             */
-/*   Updated: 2021/10/13 23:01:22 by stycho           ###   ########.fr       */
+/*   Updated: 2021/10/14 12:39:36 by stycho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -24,8 +25,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		s_pos = (char *)s;
 		req_l = 0;
 		s_copy_start = NULL;
-		while (*(s_pos++ + start) && len--)
-			req_l++;
+		if (!((unsigned int)ft_strlen(s) <= start))
+			while (*(s_pos++ + start) && len--)
+				req_l++;
 		s_copy = (char *)malloc((req_l + 1) * sizeof(char));
 		if (s_copy)
 		{
