e8cd4b4a95fd530151130f6c6aa8436cd3dd3632
stycho <stycho@student.21-school.ru>
2021-10-14 12:52:18 +0300

    ft_strjoin now protected

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: stycho <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 18:46:13 by stycho            #+#    #+#             */
-/*   Updated: 2021/10/11 18:46:15 by stycho           ###   ########.fr       */
+/*   Updated: 2021/10/14 12:51:39 by stycho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,22 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*res;
 	char	*res_start;
 
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	res = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
-	if (res)
+	if (s1 && s2)
 	{
-		res_start = res;
-		while (len_s1--)
-			*res++ = *s1++;
-		while (len_s2--)
-			*res++ = *s2++;
-		*res = '\0';
-		return (res_start);
+		len_s1 = ft_strlen(s1);
+		len_s2 = ft_strlen(s2);
+		res = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+		if (res)
+		{
+			res_start = res;
+			while (len_s1--)
+				*res++ = *s1++;
+			while (len_s2--)
+				*res++ = *s2++;
+			*res = '\0';
+			return (res_start);
+		}
+		return (res);
 	}
-	return (res);
+	return (NULL);
 }
