a3a09b756a7d6b131c56eeaab8167a628922a9b0
stycho <stycho@student.21-school.ru>
2021-10-15 23:02:55 +0300

    split passes all tests

--- ft_split.c
+++ ft_split.c
@@ -5,18 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stycho <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/13 10:42:12 by stycho            #+#    #+#             */
-/*   Updated: 2021/10/15 12:55:57 by                  ###   ########.fr       */
+/*   Created: 2021/10/15 22:02:33 by stycho            #+#    #+#             */
+/*   Updated: 2021/10/15 22:02:34 by stycho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "libft.h"
 
 static size_t	ft_count_words(const char *raw_str, char sp);
 static char		*ft_all_mem_for_word(const char *p_to_raw_str, char term_symb);
 static void		ft_copy_word(char *dst, const char *src, char term_symb);
-//static char     **ft_check_leaks(char **arr_of_strings, size_t str_count);
+static char		**ft_check_leaks(char **arr_of_strings, size_t i);
 
 char	**ft_split(char	const *s, char c)
 {
@@ -25,26 +24,24 @@ char	**ft_split(char	const *s, char c)
 	char	**splitted_words;
 	char	*word;
 
-	splitted_words = NULL;
-	if (s)
+	if (!(s))
+		return (NULL);
+	word_count = ft_count_words(s, c);
+	splitted_words = (char **)ft_calloc((word_count + 1), sizeof(char *));
+	i = 0;
+	while (*s && i < word_count)
 	{
-		word_count = ft_count_words(s, c);
-		splitted_words = (char **)malloc(word_count * sizeof(char *) + 1);
-		i = 0;
-		while (*s && word_count)
-		{
-			while (*s == c)
-				s++;
-			word = ft_all_mem_for_word(s, c);
-			splitted_words[i] = word;
-			ft_copy_word(word, s, c);
-			while (*s != c)
-				s++;
-			i++;
-			word_count--;
-		}
-		splitted_words[i] = NULL;
-//		splitted_words = ft_check_leaks(splitted_words, i);
+		while (*s && *s == c)
+			s++;
+		if (!*s)
+			break ;
+		word = ft_all_mem_for_word(s, c);
+		if (!(word))
+			return (ft_check_leaks(splitted_words, i));
+		splitted_words[i++] = word;
+		ft_copy_word(word, s, c);
+		while (*s && *s != c)
+			s++;
 	}
 	return (splitted_words);
 }
@@ -76,8 +73,11 @@ static char	*ft_all_mem_for_word(const char *p_to_raw_str, char term_symb)
 
 	copy_ptrawstr = p_to_raw_str;
 	w_len = 0;
-	while (*copy_ptrawstr++ != term_symb)
+	while (*copy_ptrawstr && *copy_ptrawstr != term_symb)
+	{
 		w_len++;
+		copy_ptrawstr++;
+	}
 	p_to_blank_word = (char *)ft_calloc((w_len + 1), sizeof(char));
 	return (p_to_blank_word);
 }
@@ -87,26 +87,14 @@ static void	ft_copy_word(char *dst, const char *src, char term_symb)
 	char	*copy_src;
 
 	copy_src = (char *)src;
-	while (*copy_src != term_symb)
-		*dst++ = *copy_src++;
+	while (*copy_src && *copy_src != term_symb)
+		*(dst++) = *(copy_src++);
 }
 
-//static char     **ft_check_leaks(char **arr_of_strings, size_t str_count)
-//{
-//    size_t check_count;
-//
-//    check_count = str_count;
-//    while (--check_count)
-//    {
-//        if (arr_of_strings[check_count] == NULL)
-//        {
-//            while (str_count)
-//            {
-//                free(arr_of_strings[str_count - 1]);
-//                str_count--;
-//            }
-//            return (NULL);
-//        }
-//    }
-//    return (arr_of_strings);
-//}
+static char	**ft_check_leaks(char **arr_of_strings, size_t i)
+{
+	while (i--)
+		free(arr_of_strings[i]);
+	free(arr_of_strings);
+	return (NULL);
+}
