6c60a1de74af420a453246ec562cd7a7224720bd
stycho <stycho@student.21-school.ru>
2021-10-15 12:21:00 +0300

    rewritten ft_strnstr.c that passes all checkers

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,18 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By:  <stycho>                                   +#+  +:+       +#+       */
+/*   By: stycho <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/15 01:05:44 by                   #+#    #+#             */
-/*   Updated: 2021/10/15 01:08:08 by                  ###   ########.fr       */
+/*   Created: 2021/10/08 20:53:14 by stycho            #+#    #+#             */
+/*   Updated: 2021/10/15 12:17:19 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#include <stddef.h>
+#include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	ndl_len;
 
-
+	if (!(*needle))
+		return ((char *)haystack);
+	ndl_len = ft_strlen(needle);
+	while (len && *(haystack + ndl_len - 1) && *haystack)
+	{
+		if (ndl_len <= len)
+			if ((ft_strncmp(haystack, needle, ndl_len) == 0))
+				return ((char *)haystack);
+		len--;
+		haystack++;
+	}
+	return (NULL);
 }
